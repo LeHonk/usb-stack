e22f081af7b0d9019535b109caad16c32ef209ab
Tomas Holmqvist <LeHonk@users.noreply.github.com>
2012-12-11 09:05:36 +0100

    Moved master (svn trunk) into proper

--- branches/pingpong/cdc/cdc.d
+++
@@ -1,2 +0,0 @@
-cdc.d cdc.o: cdc.c ../core/usb_stack.h ../core/usb_lang.h usb_config.h \
-  ../core/usb_p24f.h descriptors.h cdc.h
--- branches/pingpong/cdc/usb_stack.d
+++
@@ -1,2 +0,0 @@
-usb_stack.d usb_stack.o: ../core/usb_stack.c ../core/usb_stack.h \
-  ../core/usb_lang.h usb_config.h ../core/usb_p24f.h
